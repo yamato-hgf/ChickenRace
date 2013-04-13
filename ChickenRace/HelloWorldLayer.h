6aef5bc440e6079268f0ae1bcb8407f6c8224708
ogawam <gm.lrhv@gmail.com>
2013-04-14 06:28:31 +0900

    Initial Commit

---
+++ ChickenRace/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  ChickenRace
+//
+//  Created by 小川 穣 on 2013/04/14.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
